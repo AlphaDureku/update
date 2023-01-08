c72398334b88e219a63790d885bc6370c6548ab5
unknown <templanzamark2002@gmail.com>
2023-01-09 02:23:19 +0800

    production

--- Models/sequelize_sequelizeModel.js
+++ Models/sequelize_sequelizeModel.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const Sequelize = require('sequelize')
 const { DataTypes } = Sequelize
 
-const sequelize = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
+const sequelize = new Sequelize(process.env.DB_NAME2, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     define: {
         freezeTableName: true,
